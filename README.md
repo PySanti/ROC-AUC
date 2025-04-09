807157c6114d494c1b1609b2b5892e373af816d5
santiago <santiagodeandrade4@gmail.com>
2025-04-09 11:09:25 -0400

    Commit post-entrenamiento

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ROC-AUC
 
-Este será un proyecto de clasificación en donde el principal objetivo será entender la utilidad de las técnicas de evaluación de algoritmos de aprendizaje supervisado denominadas como curvas `ROC` y `AUC`.
+Este será un proyecto de clasificación en donde el principal objetivo será entender la utilidad de las técnicas de evaluación de algoritmos de aprendizaje supervisado denominadas como curvas `ROC-AUC`.
 
 El [dataset](https://www.kaggle.com/datasets/rikdifos/credit-card-approval-prediction/discussion/119320) a utilizar contiene información sobre clientes de tarjetas de crédito. La idea básicamente es crear un modelo de aprendizaje supervisado que clasifique en "buenos clientes" y "malos clientes".
 
@@ -36,7 +36,7 @@ CNT_FAM_MEMBERS
 
 ### Duplicados 
 
-Si se elimina la columna `ID`, el dataset presenta 348.472 elementos duplicados (79.45%).
+Si se elimina la columna `ID`, el dataset presenta 348.472 elementos duplicados (79.45%). Se eliminan.
 
 
 
@@ -110,10 +110,38 @@ Finalmente, las etiquetas resultaron de la siguiente manera:
 ```
 0 : 74398 (82.59%)
 1 : 15687 (17.41%)
+```
+
+Lo anterior se obtuvo realizando estrategias de preprocesamiento sobre todo el conjunto de datos unicamente para poder visualizar. Sin embargo, posteriormente se realizo un proceso de preprocesamiento correcto, primero dividiendo el conjunto de datos y luego aplicando transformadores necesarios para poder utilizar `K-MEANS` sobre los dos conjuntos independientemente.  Estos fueron los resultados:
+
 
 ```
+Balance de clases en conjunto de train:
+
+0 : 59614 (82.71909862907255)
+1 : 12454 (17.280901370927456)
+
+Balance de clases en conjunto de test:
+
+0 : 14784 (82.05583615474275)
+1 : 3233 (17.944163845257258)
+
+Rendimiento del modelo para conjunto de train
+Clase negativa : 0.999
+Clase positiva : 0.995
+
+Rendimiento del modelo para conjunto de test
+Clase negativa : 0.999
+Clase positiva : 0.994
+```
+
+Por alguna razon, se mantiene la misma razon de balance de clases en ambos conjuntos sin haberlo hecho explicitamente (?).
+
+
 
 ## Entrenamiento
 
+El algoritmo que utilizamos para realizar pruebas fue `Naive bayes`
 
 ## Evaluación
+
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from pandas._libs.writers import word_len
+from sklearn.metrics import precision_recall_curve
+import numpy as np
 import pandas as pd
 from utils.basic_preprocess import basic_preprocess
 from utils.generate_tags import generate_tags
+import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.metrics import f1_score
@@ -22,13 +26,47 @@ target_unbalance(df_test, TARGET)
 model = BernoulliNB()
 model.fit(df_train.drop(TARGET, axis=1), df_train[TARGET])
 
-print("Rendimiento del modelo para conjunto de train")
-print(f"Clase negativa : {f1_score(df_train[TARGET], model.predict(df_train.drop(TARGET, axis=1)), pos_label=0):.3f}")
-print(f"Clase positiva : {f1_score(df_train[TARGET], model.predict(df_train.drop(TARGET, axis=1)), pos_label=1):.3f}")
+def find_best_threshold():
+    y_scores = model.predict_proba(df_test.drop(TARGET, axis=1))[:, 1]  
+    precision, recall, thresholds = precision_recall_curve(df_test[TARGET], y_scores)  
+    f1_scores = 2 * (precision * recall) / (precision + recall + 1e-10) # prevenir division por cero  
+    best_index = np.argmax(f1_scores)  
+    best_threshold = thresholds[best_index]
+    return best_threshold
+
+def model_performance(threshold=None):
+    if threshold:
+        y_scores = model.predict_proba(df_test.drop(TARGET, axis=1))[:, 1]  
+        y_pred_custom = (y_scores >= threshold).astype(int)  
+        p_class = f1_score(df_test[TARGET], y_pred_custom, pos_label=1)
+        n_class = f1_score(df_test[TARGET], y_pred_custom, pos_label=0)
+
+
+    else:
+        predictions = model.predict(df_test.drop(TARGET, axis=1))
+        p_class = f1_score(df_test[TARGET], predictions, pos_label=1)
+        n_class = f1_score(df_test[TARGET], predictions, pos_label=0)
+
+    print(f"Clase positiva : {p_class:.3f}")
+    print(f"Clase negativa : {n_class:.3f}")
+
+y_scores = model.predict_proba(df_test.drop(TARGET, axis=1))[:, 1]  
+
+best_thr = find_best_threshold()
+worst_thr = 0.5
+
+y_pred_custom_best = np.array((y_scores >= best_thr).astype(int))
+print(f"Cantidad de 1s para el mejor umbral : {np.sum(y_pred_custom_best==1)}")
+print(f"Cantidad de 0s para el mejor umbral : {np.sum(y_pred_custom_best==0)}")
+y_pred_custom_worst = np.array((y_scores >= worst_thr).astype(int))
+print(f"Cantidad de 1s para el peor umbral : {np.sum(y_pred_custom_worst==1)}")
+print(f"Cantidad de 0s para el peor umbral : {np.sum(y_pred_custom_worst==0)}")
+y_pred = model.predict(df_test.drop(TARGET, axis=1))
+print(f"Cantidad de 1s para el umbral clasico: {np.sum(y_pred==1)}")
+print(f"Cantidad de 0s para el umbral clasico: {np.sum(y_pred==0)}")
 
 
 
-print("Rendimiento del modelo para conjunto de test")
-print(f"Clase negativa : {f1_score(df_test[TARGET], model.predict(df_test.drop(TARGET, axis=1)), pos_label=0):.3f}")
-print(f"Clase positiva : {f1_score(df_test[TARGET], model.predict(df_test.drop(TARGET, axis=1)), pos_label=1):.3f}")
+
+
 
